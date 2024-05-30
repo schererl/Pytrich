aa7fb895e5ddd4fe1d2bb9ac7cc546845e9a29f2
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2024-05-30 18:20:02 +0100

    Added paper reference for loop detection.

--- pyperplan/heuristics/loop_detection_heuristic.py
+++ pyperplan/heuristics/loop_detection_heuristic.py
@@ -3,7 +3,8 @@ from heuristics.heuristic import Heuristic
 
 class LoopDetecttionHeuristic(Heuristic):
     """Heuristic that detects loops and returns infinity if a loop is detected.
-    Key contribution from Fan
+    Key contribution from Fan, based on the paper:
+    https://ojs.aaai.org/index.php/ICAPS/article/view/15959
 
     Args:
         Heuristic (_type_): _description_
